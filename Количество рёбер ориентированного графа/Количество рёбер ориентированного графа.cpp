00984c318eba3ede63adb68c401159def5443a92
Dev-C-Pascal <lesnikovevgen7965@gmail.com>
2022-01-14 20:30:55 +0200

    Количество рёбер ориентированного графа

---
+++ Количество рёбер ориентированного графа/Количество рёбер ориентированного графа.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+ 
+ 
+using namespace std;
+ 
+ 
+ 
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n, k = 0,size;
+    cin >> n;
+    int *mas = new int[n*n];
+    for (int i = 0; i < n*n; i++) {
+        cin >> mas[i];
+        if (mas[i] == 1)
+            k++;
+    }
+    
+    cout << k << endl;
+    system("pause");
+    return 0;
+ 
+}
